3a8cafbe52ddc06cd99cb593c82159b5caef9cc8
Ryan <ryanhougaske@gmail.com>
2025-03-07 15:12:35 -0400

    Feat: add table Role and relationship between it and the users table

--- src/main/java/com/example/Project_Web_Spring/entity/Role.java
+++ src/main/java/com/example/Project_Web_Spring/entity/Role.java
@@ -1,45 +1,31 @@
 package com.example.Project_Web_Spring.entity;
 
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "usuarios")
-public class User {
-    
-    @Id
+public class Role {
+        @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
-    // @ManyToMany
-    // private List<Role> roles;
-    @ManyToMany
-    private Set<Role> roles;
     private String name;
-    private String email;
-     
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
+
 }
