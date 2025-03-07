6c3c519bef317671c9be766213396ececccb9ebc
Ryan <ryanhougaske@gmail.com>
2025-03-07 15:00:23 -0400

    Fix: Table conflict resolved

--- src/main/java/com/example/Project_Web_Spring/entity/User.java
+++ src/main/java/com/example/Project_Web_Spring/entity/User.java
@@ -1,12 +1,14 @@
-package entity;
+package com.example.Project_Web_Spring.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-public class user {
+@Table(name = "usuarios")
+public class User {
     
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
